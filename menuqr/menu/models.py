7f12681ab50f8187c2d55e7af9a6f46666f2a91b
adityadesity <density1912@gmail.com>
2026-01-13 23:36:05 +0530

    added models

--- menuqr/menu/models.py
+++ menuqr/menu/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from restaurants.models import Restaurants
 # Create your models here.
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100)
+    restaurant_id = models.ForeignKey(Restaurants,on_delete=models.CASCADE)
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    category_id = models.ForeignKey(MenuCategory,on_delete=models.CASCADE)
--- menuqr/restaurants/models.py
+++ menuqr/restaurants/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from accounts.models import RestaurantOwner
 # Create your models here.
+
+class Restaurants(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    owner_id = models.ForeignKey(to=RestaurantOwner,on_delete=models.CASCADE)
